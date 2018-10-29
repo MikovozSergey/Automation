83247b5808d0dd1bce02b65bf6456a78c7a5b8ed
MikovozSergey <mikovozs98@gmail.com>
2018-10-29 20:03:59 +0300

    Add labb WebDriver Selenium

---
+++ lab4/WebDriver_Example/WebDriver_Example/Program.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDriver_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://www.britishairways.com/travel/home/public/en_us";
+
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(3));
+
+            driver.FindElement(By.Id("planTripFlightDestination")).SendKeys("Frankfurt, Frankfurt (Fra), Germany");
+            driver.FindElement(By.ClassName("calendarIcon depDate")).Click();
+            driver.FindElement(By.XPath("//*[@ui-state-default ui-state-active='31']")).Click();
+            driver.FindElement(By.ClassName("calendarIcon retDate")).Click();
+            driver.FindElement(By.XPath("//*[@ui-state-default ui-state-active='7']")).Click();
+            driver.FindElement(By.Id("flightSearchButton")).Click();
+
+        }
+    }
+}
