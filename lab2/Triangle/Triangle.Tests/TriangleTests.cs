26296e1d76b02cec8c15ac653aea1d1c8b394669
MikovozSergey <mikovozs98@gmail.com>
2018-11-02 09:37:13 +0300

    fix some tests

--- lab2/Triangle/Triangle.Tests/TriangleTests.cs
+++ lab2/Triangle/Triangle.Tests/TriangleTests.cs
@@ -1,56 +1,33 @@
 ﻿using System;
-
 using NUnit.Framework;
-
 
 namespace Triangle.Tests
 {
-    
-			[TestFixture]
-    
-public class TriangleTests
-{
-       
-			[TestCase(7, 7, 7, ExpectedResult = true)]
-        
-			[TestCase(6, 7, 7, ExpectedResult = true)]
-        
-			[TestCase(21, 23, 22, ExpectedResult = true)]
-       
-			[TestCase(1, 3, 10, ExpectedResult = true)]
-        
-			[TestCase(11, 10, 96, ExpectedResult = true)]
-            
-			[TestCase(2.312, 7.012, 10.056, ExpectedResult = true)]
-       
-public bool Triangle_InputNumbers_False(double a, double b, double c)
-           
-			=> Triangle.IsTriangle(a, b, c);
+    [TestFixture]
+    public class TriangleTests
+    {
+        [TestCase(5, 5, 5, ExpectedResult = true)]
+        [TestCase(5, 6, 6, ExpectedResult = true)]
+        [TestCase(5, 10, 11, ExpectedResult = true)]
+        [TestCase(2, 2, 3, ExpectedResult = true)]
+        [TestCase(1, 100, 100, ExpectedResult = true)]
+        [TestCase(12, 17, 28, ExpectedResult = true)]
+        [TestCase(3, 5, 5, ExpectedResult = true)]
+        [TestCase(3.523, 5.123, 5.765, ExpectedResult = true)]
+        public bool Triangle_InputNumbers_True(double a, double b, double c)
+            => Triangle.IsTriangle(a, b, c);
 
-        
-			[TestCase(-3, 2, 3)]
-        
-			[TestCase(3, -2, 3)]
-        
-			[TestCase(3, 3, -3)]
- 
-public bool Triangle_InputNumbers_True(double a, double b, double c)
-           
-	 => Triangle.IsTriangle(a, b, c);
+        [TestCase(3, 5, 15, ExpectedResult = false)]
+        [TestCase(1, 2, 3, ExpectedResult = false)]
+        [TestCase(5.5, 3.6, 10.3, ExpectedResult = false)]
+        [TestCase(2, 7, 3, ExpectedResult = false)]
+        public bool Triangle_InputNumbers_False(double a, double b, double c)
+           => Triangle.IsTriangle(a, b, c);
 
-        
-			[TestCase(5, 10, 15, ExpectedResult = false)]
-       
-			[TestCase(3, 2, 1, ExpectedResult = false)]
-        
-			[TestCase(2.5, 4.6, 12.3, ExpectedResult = false)]
-       
-			[TestCase(3, 8, 5, ExpectedResult = false)]
-               
-public void Triangle_IncorrectNumbers_ThrowsArgumentException(double a, double b, double c)
-                
-	=> Assert.Throws<ArgumentException>(() => Triangle.IsTriangle(a, b, c));
-    
-			}
-
+        [TestCase(-2, 1, 2)]
+        [TestCase(2, -1, 2)]
+        [TestCase(2, 1, -2)]
+        public void Triangle_IncorrectNumbers_ThrowsArgumentException(double a, double b, double c)
+                => Assert.Throws<ArgumentException>(() => Triangle.IsTriangle(a, b, c));
+    }
 }
