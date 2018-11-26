df6edff49ed64e1a3cf038fe77f2bc0cf3f94f36
MikovozSergey <mikovozs98@gmail.com>
2018-11-26 17:40:47 +0300

    some changes in ginignore

---
+++ PageObject/Page/HomePage.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Threading;
+
+namespace AirlinesTestingApp.Pages
+{
+    public class HomePage
+    {
+        private IWebDriver driver;
+        private const string url = "https://www.britishairways.com/travel/home/public/en_us";
+        By addPassenger = By.Id("sb-Flight-pax-mix");
+        By plusBaby = By.CssSelector(".disable:nth-child(2)");
+        By errorsMessages = By.ClassName("messages");
+
+        public HomePage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void OpenHomePage()
+        {
+            driver.Navigate().GoToUrl(Url);
+        }
+
+        public void AddPassengers(){
+            driver.FindElement(addPassenger).Click();
+        }
+
+        public void PlusBaby(int count = 1)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                driver.FindElement(plusBaby).Click();
+            }
+        }
+
+        public IWebElement GetErrorsMessages()
+        {
+            return driver.FindElement(errorsMessages);
+        }
+    }
+}
